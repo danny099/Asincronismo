3d1eaefd6b32c3af0e7fb619ed2d24026cde4d3e
danny099 <dalmariv01@gmail.com>
2020-07-16 23:54:45 -0500

    notas sobre asincronismo en javascript

---
+++ src/async/index.js
@@ -0,0 +1,30 @@
+const doSomethingAsync = () => {
+  return new Promise((resolve, reject) => {
+    (true)
+      ? setTimeout(() => resolve('Async'), 3000)
+      : reject(new Error('error'))
+  })
+}
+
+const doSomething = async () => {
+  const something = await doSomethingAsync()
+  console.log(something)
+}
+
+console.log('before')
+doSomething()
+console.log('after')
+
+
+const anotherFunction = async () =>{
+  try {
+    const something = await doSomethingAsync();
+    console.log(something)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+console.log('before1')
+anotherFunction()
+console.log('after2')
